bc1785c52df1faba585eb047888624ed6931e52d
jhc23 <jhc23@ic.ac.uk>
2026-01-15 17:28:15 +0000

    style: remove unused `LayoutProps`

--- frontend/src/layout/Layout.tsx
+++ frontend/src/layout/Layout.tsx
@@ -1,10 +1,6 @@
 import Navbar from '../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 
-type LayoutProps = {
-  children: React.ReactNode
-}
-
 const Layout = () => {
   return (
     <div className="flex min-h-screen bg-neutral-950 text-white">

23b793566e73c1c5feba00401a7399050c713446
Kee Meng <49086005+KeeMeng@users.noreply.github.com>
2026-01-19 16:47:58 +0000

    [kmt123] linting

--- frontend/src/components/OptionsBar.tsx
+++ frontend/src/components/OptionsBar.tsx
@@ -81,7 +81,7 @@ export function OptionsBar({
 
       // const slider = sliderRef.current.noUiSlider
       // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      const slider = (sliderRef.current as any).noUiSlider;
+      const slider = (sliderRef.current as any).noUiSlider
 
       // prevent min slider from being set to 0
       slider.on(

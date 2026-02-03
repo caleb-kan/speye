ce37c695ce8f13f611552b9be3dc30796df8d8a4
akc123 <alexander.chow23@imperial.ac.uk>
2026-02-03 16:30:01 +0000

    Resolve "Change default wpm to something slightly higher"

--- frontend/src/constants/wpm.ts
+++ frontend/src/constants/wpm.ts
@@ -1,4 +1,4 @@
-export const WPM_PRESETS = [100, 200, 300, 400]
-export const DEFAULT_WPM = 200
+export const WPM_PRESETS = [200, 400, 600, 800]
+export const DEFAULT_WPM = 400
 export const MIN_WPM = 10
 export const MAX_WPM = 2000
